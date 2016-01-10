aa9cb82e899198e32a9374205a22eefe32baf741
Horia Toma <toma.horia@gmail.com>
2016-01-10 19:44:51 +0100

    Move ball and player

--- phutball/Script1.fsx
+++ phutball/Script1.fsx
@@ -9,8 +9,6 @@ type MoveType =
     | Player
     | Ball
 
-type Position = Position of int*int
-
 type BallDirection = 
     | NorthWest
     | North
@@ -21,15 +19,21 @@ type BallDirection =
     | SouthWest
     | West
 
+type GameState =
+    | On
+    | Over
+
 let printElement (v: BoardElement) = 
     match v with
     | BoardElement.Empty -> "."
     | BoardElement.Player -> "x"
     | BoardElement.Ball -> "o"
 
+let boardDimensions (board: BoardElement[,]) =
+    board |> Array2D.length1, board |> Array2D.length2
+
 let print board = 
-    let n = board |> Array2D.length1
-    let m = board |> Array2D.length2
+    let n,m = boardDimensions board
     for i in [0..n-1] do
         printf "|"
         for j in [0..m-1] do
@@ -51,43 +55,106 @@ let findBall (board: BoardElement[,]) =
     |> Seq.find (fun (_,_,el) -> el=BoardElement.Ball)
     |> fun (i,j,_) -> (i,j)
 
-let availablePositions (board: BoardElement[,]) (moveType: MoveType) =
-    match moveType with
-    | MoveType.Player ->
-        board
-        |> Array2D.mapi (fun i j el -> if el=BoardElement.Empty then (i,j) else (-1,j))
-        |> Seq.cast<int*int>
-        |> Seq.filter (fun (i,j) -> i>=0)
-    | MoveType.Ball ->
-        let n = board |> Array2D.length1
-        let m = board |> Array2D.length2
-        let i,j = findBall board        
-        match i,j with
-        | 0,0 -> 
-            [BallDirection.East; BallDirection.SouthEast; BallDirection.South]
-        | 0,m-1 ->
-            [BallDirection.South; BallDirection.SouthWest; BallDirection.West]
-        | 0,_ -> 
-            [BallDirection.East; BallDirection.SouthEast; BallDirection.South; BallDirection.SouthWest; BallDirection.West]
-        | n-1,0 ->
-        | n-1,m-1 ->
-        | n-1, _ ->
-        | _, 0 ->
-        | _, m-1 ->
-        | _ ->
+let possibleBallPositions (board: BoardElement[,]) =
+    let i,j = findBall board
+    let rec avance (direction: BallDirection) x y =
+        if x>=0 && y>=0 && board.[x,y]=BoardElement.Player then
+            match direction with
+            | BallDirection.NorthWest ->
+                avance direction (x-1) (y-1)
+            | BallDirection.North ->
+                avance direction (x-1) y
+            | BallDirection.NorthEast ->
+                avance direction (x-1) (y+1)
+            | BallDirection.East ->
+                avance direction x (y+1)
+            | BallDirection.SouthEast ->
+                avance direction (x+1) (y+1)
+            | BallDirection.South ->
+                avance direction (x+11) y
+            | BallDirection.SouthWest ->
+                avance direction (x+1) (y-1)
+            | BallDirection.West ->
+                avance direction x (y-1)
+        else
+            x,y
+    [BallDirection.NorthWest
+     BallDirection.North
+     BallDirection.NorthEast
+     BallDirection.East
+     BallDirection.SouthEast
+     BallDirection.South
+     BallDirection.SouthWest
+     BallDirection.South]
+     |> List.map (fun direction -> avance direction i j)
+     |> List.filter (fun (x,y) -> x<>i && y<>j)
 
-let move (board: BoardElement[,]) (moveType: MoveType) (position: Position) =
-    let (Position (x,y)) = position
+let possiblePlayerPositions (board: BoardElement[,]) =
+    board
+    |> Array2D.mapi (fun i j el -> if el=BoardElement.Empty then (i,j) else (-1,j))
+    |> Seq.cast<int*int>
+    |> Seq.filter (fun (i,j) -> i>=0)
+    |> List.ofSeq
+
+let moveBall (board: BoardElement[,]) (x,y) = 
+    let a,b = findBall board
+    let n,m = boardDimensions board
+    if a=x && b=y then GameState.On,board
+    else
+        if x<0 || x=n || y<0 || y=m then
+            GameState.Over,board
+        else
+            let direction =
+                if x=a then
+                    if y>b then BallDirection.East else BallDirection.West
+                elif y=b then
+                    if x<a then BallDirection.North else BallDirection.South
+                elif x<a then
+                    if y<b then BallDirection.NorthWest else BallDirection.NorthEast
+                else
+                    if y<b then BallDirection.SouthWest else BallDirection.SouthEast
+            match direction with
+             | BallDirection.NorthWest ->
+                for i in [x+1..a-1] do
+                    board.[i,y-(a-i)]<-BoardElement.Empty
+             | BallDirection.North ->
+                for i in [a+1..x-1] do
+                    board.[i,b]<-BoardElement.Empty
+             | BallDirection.NorthEast ->
+                for i in [a+1..x-1] do
+                    board.[i,y-(a-i)]<-BoardElement.Empty
+             | BallDirection.East ->
+                for j in [b+1..y-1] do
+                    board.[x,j]<-BoardElement.Empty
+             | BallDirection.SouthEast ->
+                for i in [a+1..x-1] do
+                    board.[i,y+(a-i)]<-BoardElement.Empty
+             | BallDirection.South ->
+                for i in [x+1..a-1] do
+                    board.[i,b]<-BoardElement.Empty
+             | BallDirection.SouthWest ->
+                for i in [a+1..x-1] do
+                    board.[i,y-(a-i)]<-BoardElement.Empty
+             | BallDirection.West ->
+                for j in [y+1..b-1] do
+                    board.[x,j]<-BoardElement.Empty 
+                    
+            GameState.On,board
+        
+
+let move (board: BoardElement[,]) (moveType: MoveType) (x,y) =
     match board.[x,y] with
     | BoardElement.Empty -> failwith "Position already occupied"
     | _ ->
         match moveType with
         | MoveType.Player -> 
             board.[x,y]<-BoardElement.Player
+            GameState.On,board
         | MoveType.Ball -> 
-            board.[x,y]<-BoardElement.Ball
-        board
-                 
+            moveBall board (x,y)
+
+let play (board: BoardElement[,]) = 
+    
 
 let board = initialize (19,15)
 print board
